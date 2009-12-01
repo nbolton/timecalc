76c9fb8a6ddf70f3b3178663039ff244ba6ae4c4
Nick Bolton <nick@synergy-project.org>
2009-12-01 04:44:13 +0000

    Initial commit

---
+++ TimeCalc/MainForm.Designer.cs
@@ -0,0 +1,189 @@
+﻿namespace TimeCalc
+{
+    partial class MainForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(MainForm));
+            this.label1 = new System.Windows.Forms.Label();
+            this.startTextBox = new System.Windows.Forms.TextBox();
+            this.endTextBox = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.calcButton = new System.Windows.Forms.Button();
+            this.hoursTextBox = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.decTextBox = new System.Windows.Forms.TextBox();
+            this.groupBox1.SuspendLayout();
+            this.groupBox2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(13, 22);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(32, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Start:";
+            // 
+            // startTextBox
+            // 
+            this.startTextBox.Location = new System.Drawing.Point(78, 19);
+            this.startTextBox.Name = "startTextBox";
+            this.startTextBox.Size = new System.Drawing.Size(67, 20);
+            this.startTextBox.TabIndex = 1;
+            // 
+            // endTextBox
+            // 
+            this.endTextBox.Location = new System.Drawing.Point(78, 44);
+            this.endTextBox.Name = "endTextBox";
+            this.endTextBox.Size = new System.Drawing.Size(67, 20);
+            this.endTextBox.TabIndex = 3;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(13, 47);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(29, 13);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "End:";
+            // 
+            // calcButton
+            // 
+            this.calcButton.Location = new System.Drawing.Point(78, 70);
+            this.calcButton.Name = "calcButton";
+            this.calcButton.Size = new System.Drawing.Size(67, 23);
+            this.calcButton.TabIndex = 4;
+            this.calcButton.Text = "Calculate";
+            this.calcButton.UseVisualStyleBackColor = true;
+            this.calcButton.Click += new System.EventHandler(this.calcButton_Click);
+            // 
+            // hoursTextBox
+            // 
+            this.hoursTextBox.Location = new System.Drawing.Point(78, 22);
+            this.hoursTextBox.Name = "hoursTextBox";
+            this.hoursTextBox.Size = new System.Drawing.Size(67, 20);
+            this.hoursTextBox.TabIndex = 5;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(13, 25);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(38, 13);
+            this.label3.TabIndex = 6;
+            this.label3.Text = "Hours:";
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left)
+                        | System.Windows.Forms.AnchorStyles.Right)));
+            this.groupBox1.Controls.Add(this.startTextBox);
+            this.groupBox1.Controls.Add(this.label1);
+            this.groupBox1.Controls.Add(this.label2);
+            this.groupBox1.Controls.Add(this.calcButton);
+            this.groupBox1.Controls.Add(this.endTextBox);
+            this.groupBox1.Location = new System.Drawing.Point(12, 12);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(223, 107);
+            this.groupBox1.TabIndex = 7;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Variables";
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom)
+                        | System.Windows.Forms.AnchorStyles.Left)
+                        | System.Windows.Forms.AnchorStyles.Right)));
+            this.groupBox2.Controls.Add(this.label4);
+            this.groupBox2.Controls.Add(this.decTextBox);
+            this.groupBox2.Controls.Add(this.label3);
+            this.groupBox2.Controls.Add(this.hoursTextBox);
+            this.groupBox2.Location = new System.Drawing.Point(12, 125);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(223, 84);
+            this.groupBox2.TabIndex = 8;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "Result";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(13, 51);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(48, 13);
+            this.label4.TabIndex = 8;
+            this.label4.Text = "Decimal:";
+            // 
+            // decTextBox
+            // 
+            this.decTextBox.Location = new System.Drawing.Point(78, 48);
+            this.decTextBox.Name = "decTextBox";
+            this.decTextBox.Size = new System.Drawing.Size(67, 20);
+            this.decTextBox.TabIndex = 7;
+            // 
+            // MainForm
+            // 
+            this.AcceptButton = this.calcButton;
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(247, 221);
+            this.Controls.Add(this.groupBox2);
+            this.Controls.Add(this.groupBox1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "MainForm";
+            this.Text = "Time Calculator";
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.groupBox2.ResumeLayout(false);
+            this.groupBox2.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox startTextBox;
+        private System.Windows.Forms.TextBox endTextBox;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button calcButton;
+        private System.Windows.Forms.TextBox hoursTextBox;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox decTextBox;
+    }
+}
+
---
+++ TimeCalc/MainForm.cs
@@ -0,0 +1,107 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace TimeCalc
+{
+    public partial class MainForm : Form
+    {
+        public MainForm()
+        {
+            InitializeComponent();
+        }
+
+        private void calcButton_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                calculate();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(
+                    string.Format("Oops! {0}", ex.Message),
+                    "It broke!", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+        private string toTwentyFourHour(string unknownFormat)
+        {
+            if (unknownFormat.ToLower().EndsWith("pm")) 
+            {
+                string[] split = unknownFormat.Split(':');
+                string minutes = split[1].Substring(
+                    0, split[1].Length - 2);
+                string tweleveHourString = split[0];
+                int twentyFourHour = int.Parse(tweleveHourString) + 12;
+                return string.Format(
+                    "{0}:{1}",
+                    twentyFourHour.ToString().PadLeft(2, '0'),
+                    minutes.PadLeft(2, '0'));
+            }
+            else if (unknownFormat.ToLower().EndsWith("am")) 
+            {
+                // remove 'am' so it's now 24 hr
+                return unknownFormat.Substring(
+                    0, unknownFormat.Length - 2);
+            }
+            else
+            {
+                // guess 24 hour
+                return unknownFormat;
+            }
+        }
+
+        private void calculate()
+        {
+            startTextBox.Text = toTwentyFourHour(startTextBox.Text);
+            endTextBox.Text = toTwentyFourHour(endTextBox.Text);
+
+            string[] startSplit = startTextBox.Text.Split(':');
+            string[] endSplit = endTextBox.Text.Split(':');
+
+            int startHours = int.Parse(startSplit[0]);
+            int startMins = int.Parse(startSplit[1]);
+            int endHours = int.Parse(endSplit[0]);
+            int endMins = int.Parse(endSplit[1]);
+
+            int resultHours;
+            if (startHours > endHours) // clock elapsed?
+            {
+                resultHours = (24 - startHours) + endHours;
+            }
+            else
+            {
+                resultHours = startHours - endHours;
+            }
+
+            // merge mins and hours for accurate minute translation
+            int resultMins = (resultHours * 60) + (startMins - endMins);
+
+            // convert mins to hours, and have a remainder of mins
+            resultHours = 0;
+            while (resultMins >= 60) {
+                resultHours++;
+                resultMins -= 60;
+            }
+
+            hoursTextBox.Text = string.Format(
+                "{0}:{1}",
+                resultHours.ToString(),
+                resultMins.ToString());
+
+            // also yield a weird decimal version of mins
+            int resultMinsDec = (int)(((float)resultMins / 60) * 10);
+
+            decTextBox.Text = string.Format(
+                "{0}.{1}",
+                resultHours.ToString(),
+                resultMinsDec);
+        }
+    }
+}
